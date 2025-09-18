0b40aa1290f9491fb5da3ef5f1119cb8af904ae3
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-18 23:19:22 +0530

    Update : 769. Quadratic Equation Solver

--- CodeKata/(2) Absolute Beginner  - 30/Python/769. Quadratic Equation Solver.py
+++ CodeKata/(2) Absolute Beginner  - 30/Python/769. Quadratic Equation Solver.py
@@ -1,3 +1,5 @@
+# Before Test Case Problem
+
 import math
 
 n = input().strip().split()
@@ -6,8 +8,12 @@ b = float(n[1])
 c = float(n[2])
 
 d = b * b - 4 * a * c
-e = (-b + math.sqrt(d)) / (2 * a)
-h = (-b - math.sqrt(d)) / (2 * a)
 
-print(f"{e:.2f}")
-print(f"{h:.2f}")
+if d < 0:
+    print("Complex Roots")
+else:
+    e = (-b + math.sqrt(d)) / (2 * a)
+    h = (-b - math.sqrt(d)) / (2 * a)
+
+    print(f"{e:.2f}")
+    print(f"{h:.2f}")
