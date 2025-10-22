c3ef5c140cd673ceaec74ad3243c3b14c422f06e
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-22 22:01:59 +0530

    Solved Problem No : 55

--- CodeKata/Array - (309)/JavaScript/55. Counting Distinct Pairs.js
+++ CodeKata/Array - (309)/JavaScript/55. Counting Distinct Pairs.js
@@ -12,19 +12,21 @@ inp.on("line", (data) => {
 
 inp.on("close", () => {
 
-    let v = userInput[0].split(" ")
-    let p = userInput[1].split(" ").map(Number)
-    let vp = 0
-    let pv = [...new Set(p)].sort((a, b) => a - b);
-    //   console.log(pv)
+    let p = userInput[1].split(" ").map(Number);
+    let vp = 0;
+    let seenPairs = new Set();
 
-    for (let i = 0; i < pv.length; i++) {
-        for (let j = i + 1; j < pv.length; j++) {
-            if (pv[i] < pv[j]) {
-                console.log(p[i], p[j], p[i] < p[j])
-                vp += 1
+    for (let i = 0; i < p.length; i++) {
+        for (let j = i + 1; j < p.length; j++) {
+            if (p[i] < p[j]) {
+                let key = `${p[i]}-${p[j]}`;
+                if (!seenPairs.has(key)) {
+                    seenPairs.add(key);
+                    vp += 1;
+                }
             }
         }
     }
-    console.log(vp < 0 ? -1 : vp)
+
+    console.log(vp === 0 ? -1 : vp);
 });
