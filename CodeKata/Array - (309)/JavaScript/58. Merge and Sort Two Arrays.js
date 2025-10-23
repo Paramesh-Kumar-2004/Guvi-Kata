b39175c90b76aa1ad921dabc12a8586910f53ac1
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-23 22:01:21 +0530

    Solved Problem No:58

--- CodeKata/Array - (309)/JavaScript/58. Merge and Sort Two Arrays.js
+++ CodeKata/Array - (309)/JavaScript/58. Merge and Sort Two Arrays.js
@@ -20,3 +20,38 @@ inp.on("close", () => {
     console.log(pv.join(" "))
 
 });
+
+
+
+
+/*
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+
+  const tokens = userInput.join(" ").trim().split(/\s+/).map(Number);
+
+  let v = tokens[0] || 0;
+  let p = tokens[1] || 0;
+
+  let vv = tokens.slice(2, 2 + v);
+  let pp = tokens.slice(2 + v, 2 + v + p);
+
+  let vp = [...vv, ...pp];
+  let pv = vp.sort((a, b) => a - b);
+
+  console.log(pv.join(" "));
+});
+
+*/
