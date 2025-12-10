1476a693e202c59249c53a427b0ca058d4acbc2c
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-12-10 22:41:40 +0530

    Problem 293 - Solved : Strings

---
+++ CodeKata/String - (196)/JavaScript/293. Reverse Odd Positioned Words.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+
+const inp = readline.createInterface({
+    input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+    userInput.push(data.trim());
+});
+
+inp.on("close", () => {
+
+    let v = userInput[0].split(" ")
+    let vp = []
+
+    for (let i = 0; i < v.length; i++) {
+        if (i % 2 == 0) {
+            vp.push(v[i].split("").reverse().join(""))
+        }
+        else {
+            vp.push(v[i])
+        }
+    }
+    console.log(vp.join(" "))
+});
